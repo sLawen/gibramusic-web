6fc3c114a4d848203df3bcce5d21ef24ee57eaa3
Lawen <112584771+sLawen@users.noreply.github.com>
2024-07-05 21:45:59 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -45,8 +45,8 @@ Voter GibraMusic</a>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
           <div className="col-span-4">
-            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"MusicMaker" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
+            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"GibraMusic" }} />
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "Que diriez-vous de créer le meilleur environnement pour vous détendre avec vos propres émotions?" }}/>
             <a href="https://discord.com/oauth2/authorize?client_id=1258410048615612566&permissions=8&integration_type=0&scope=bot+applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
               <i className="fab fa-discord mr-2" />Inviter GibraMusic
             </a>
